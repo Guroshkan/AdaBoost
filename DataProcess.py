d3615b4c0cd305b84305cd57bd27993f55440209
Nikol <44338413+Guroshkan@users.noreply.github.com>
2021-08-13 21:54:31 +0300

    Add files via upload

--- DataProcess.py
+++ DataProcess.py
@@ -1,3 +1,9 @@
+# uncompyle6 version 3.7.4
+# Python bytecode 3.8 (3413)
+# Decompiled from: Python 3.8.7 (tags/v3.8.7:6503f05, Dec 21 2020, 17:59:51) [MSC v.1928 64 bit (AMD64)]
+# Embedded file name: C:\Users\acer nitro 5\PycharmProjects\AdaBoostVisualDemo lib\DataProcess.py
+# Compiled at: 2021-05-08 03:36:16
+# Size of source mod 2**32: 2970 bytes
 import numpy as np
 
 class DataProcess:
