633f873b537a33d8963ba012cda875fbe71cb853
Nikol <44338413+Guroshkan@users.noreply.github.com>
2021-08-13 21:57:37 +0300

    Update Language.py

--- Language.py
+++ Language.py
@@ -1,9 +1,3 @@
-# uncompyle6 version 3.7.4
-# Python bytecode 3.8 (3413)
-# Decompiled from: Python 3.8.7 (tags/v3.8.7:6503f05, Dec 21 2020, 17:59:51) [MSC v.1928 64 bit (AMD64)]
-# Embedded file name: C:\Users\acer nitro 5\PycharmProjects\AdaBoostVisualDemo lib\Language.py
-# Compiled at: 2021-04-23 22:24:46
-# Size of source mod 2**32: 129 bytes
 from enum import Enum
 
 lenth_label = 30
