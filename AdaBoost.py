383175b124b44e4bdcd072565bc3aa557ed18736
Nikol <44338413+Guroshkan@users.noreply.github.com>
2021-08-13 21:51:34 +0300

    Update AdaBoost.py

--- AdaBoost.py
+++ AdaBoost.py
@@ -1,9 +1,3 @@
-# uncompyle6 version 3.7.4
-# Python bytecode 3.8 (3413)
-# Decompiled from: Python 3.8.7 (tags/v3.8.7:6503f05, Dec 21 2020, 17:59:51) [MSC v.1928 64 bit (AMD64)]
-# Embedded file name: C:\Users\acer nitro 5\PycharmProjects\AdaBoostVisualDemo lib\AdaBoost.py
-# Compiled at: 2021-05-10 06:11:37
-# Size of source mod 2**32: 3836 bytes
 import numpy as np, random
 from sklearn.ensemble import AdaBoostClassifier
 from sklearn.linear_model import LogisticRegression
